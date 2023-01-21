cc060af277ee9ecfcde371070ee60fd91049095e
Shea McDevitt <shea@projectark.co.uk>
2023-01-21 22:31:16 +0000

    tst

--- frontend/src/pages/Bookshelf/index.tsx
+++ frontend/src/pages/Bookshelf/index.tsx
@@ -13,12 +13,7 @@ const Bookshelf: React.FC = () => {
         $500 loan from his father. -Have a clear vision and mission for your
         company -Be willing to take calculated risks -Be persistent and
         resilient in the face of challenges and setbacks -Be willing to adapt
-        and evolve your business model as needed -Be passionate and dedicated to
-        your work -Be open to new ideas and perspectives -Be prepared for the
-        long-term and have a long-term mindset -Be able to surround yourself
-        with a strong and talented team -Be able to make tough decisions and
-        take personal responsibility for the success or failure of the business
-        -Be willing to work hard and make sacrifices to achieve success fc
+        and evolve your business model as needed -Be passionate
       </Typography>
       <Divider />
     </Box>
